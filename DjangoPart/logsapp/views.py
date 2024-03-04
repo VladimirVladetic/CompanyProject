3dff57afb70948bd447b9e09e041f26937a258ae
VladimirVladetic <vvladetic@etfos.hr>
2024-03-04 10:14:34 +0100

    Removed libraries that were not being utilized

--- DjangoPart/logsapp/views.py
+++ DjangoPart/logsapp/views.py
@@ -1,10 +1,6 @@
-from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.response import Response
-from .serializer import LogsSerializer
-from .models import Logs
 from .services import LogsStatisticsService, LogsService
-from rest_framework import status
 from datetime import time
 
 # Create your views here.
